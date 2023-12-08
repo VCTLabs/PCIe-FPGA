dfe03333790d781dd7ae533f09d48b3e7cd288e6
Stephen L Arnold <sarnold@vctlabs.com>
2023-12-08 11:46:35 -0800

    update source files to include SPDX licensing identifiers
    * add updated copyright and license snippets
    * https://www.kernel.org/doc/html/latest/process/license-rules.html
    
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- src/driver/BBN_FPGA.c
+++ src/driver/BBN_FPGA.c
@@ -1,10 +1,30 @@
-// Copyright 2014 Raytheon BBN Technologies
-// Original Author: Colm Ryan (cryan@bbn.com)
+/*
+ * SPDX-FileCopyrightText: Copyright 2014 Raytheon BBN Technologies. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 #include "BBN_FPGA.h"
 
 
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("Colm Ryan (cryan@bbn.com)");
 MODULE_DESCRIPTION("Driver for PCI Altera FPGA boards with a PCI Qsys interface.");
 
--- src/driver/BBN_FPGA.h
+++ src/driver/BBN_FPGA.h
@@ -1,5 +1,25 @@
-// Copyright 2014 Raytheon BBN Technologies
-// Original Author: Colm Ryan (cryan@bbn.com)
+/*
+ * SPDX-FileCopyrightText: Copyright 2014 Raytheon BBN Technologies. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -33,25 +53,25 @@ static const size_t BUFFER_SIZE = PAGE_SIZE;
 struct DevInfo_t {
   /* the kernel pci device data structure */
   struct pci_dev *pciDev;
-  
+
   /* upstream root node */
   struct pci_dev *upstream;
-  
+
   /* kernel's virtual addr. for the mapped BARs */
   void * __iomem bar[NUM_BARS];
-  
+
   /* length of each memory region. Used for error checking. */
   size_t barLengths[NUM_BARS];
 
   /* temporary buffer. If allocated, will be BUFFER_SIZE. */
   char *buffer;
-  
+
   /* Mutex for this device. */
   struct semaphore sem;
-  
+
   /* PID of process that called open() */
   int userPID;
-  
+
   /* character device */
   dev_t cdevNum;
   struct cdev cdev;
